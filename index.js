9c03082ba7c078d1de1b9be637c018e722b19c74
Sara1502 <moreirasaragds@gmail.com>
2025-12-01 11:30:04 -0300

    Alerta de lista vazia

--- index.js
+++ index.js
@@ -50,4 +50,21 @@ botaoAdicionar.addEventListener("click", (evento) => {
   itemDaLista.appendChild(itemData);
 
   listaDeCompras.appendChild(itemDaLista);
+
+  verificarListaVazia();
 });
+
+
+const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
+
+function verificarListaVazia() {
+  const itensDaLista = listaDeCompras.querySelectorAll("li");
+
+  if (itensDaLista.length === 0) {
+    mensagemListaVazia.style.display = "block"
+  } else {
+    mensagemListaVazia.style.display = "none"
+  }
+}
+
+verificarListaVazia();
